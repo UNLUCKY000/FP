f0e38933328e1b4c171c0547dd2e3cc005feee34
UNLUCKY000 <62801726+UNLUCKY000@users.noreply.github.com>
2024-01-03 23:09:41 +0400

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,20 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
 model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
 
-st.title("T5 Text Translation")
+def translate_text(text):
+  input_ids = tokenizer(text, return_tensors="pt").input_ids
 
-source_text = st.text_input("Enter text to translate:")
-target_language = st.selectbox("Select target language:", ["German", "French", "Spanish", "Chinese"])
+  outputs = model.generate(input_ids)
+  return tokenizer.decode(outputs[0])
+
+
+st.title("Multi-lingual Translator")
+
+source_lang = st.selectbox("Source Language", ["English", "French", "German", "Spanish"])
+target_lang = st.selectbox("Target Language", ["English", "French", "German", "Spanish"])
+
+text = st.text_area("Enter text to translate")
 
 if st.button("Translate"):
-    input_ids = tokenizer(source_text, return_tensors="pt").input_ids
-
-    outputs = model.generate(input_ids)
-    translated_text = tokenizer.decode(outputs[0])
-
-    st.write(f"Translated text ({target_language}): {translated_text}")
+  st.write("Translation:")
+  st.write(translate_text(text))
